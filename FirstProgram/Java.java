c25c47d065c03c07af1f3ebb3f4b5d12aeb6a429
erameco <133159163+erameco@users.noreply.github.com>
2023-05-20 15:34:15 +0200

    first

---
+++ FirstProgram/Java.java
@@ -0,0 +1,17 @@
+public class Java{
+    public static void main(String[] args) {
+        System.out.println("My name is Era");
+        System.out.println("I am 18 years old");
+        System.out.println("My hometown is Korca");
+
+
+
+    }
+
+
+
+
+
+
+
+}
